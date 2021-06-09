92c7aaf38a168d1ded070477fbbfbcdd88796406
skullwarriorsergio <73899511+skullwarriorsergio@users.noreply.github.com>
2021-06-09 10:59:07 -0400

    Add host proccess

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+//
+var http = require('http');
+http.createServer(function (req, res)
+{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.send('Bot is running\n');
+}).listen(process.env.PORT || 5000);
 
 //-----------Requires-----------
 require('dotenv').config({path: __dirname + '/.env'});
