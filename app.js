fc240f712a6c98c5beca9435506c81ef71347bc0
skullwarriorsergio <73899511+skullwarriorsergio@users.noreply.github.com>
2021-06-09 11:00:58 -0400

    Add host proccess

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 //
 var http = require('http');
 http.createServer(function (req, res)
-{
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.send('Bot is running\n');
+{    
 }).listen(process.env.PORT || 5000);
 
 //-----------Requires-----------
