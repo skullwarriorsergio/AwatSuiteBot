4878ab94c79fb94e89ec959f61e5e4611379803e
skullwarriorsergio <73899511+skullwarriorsergio@users.noreply.github.com>
2021-06-13 21:25:04 -0400

    Adding img data for Report

--- modules/msgPicture.js
+++ modules/msgPicture.js
@@ -1,12 +1,15 @@
 //-----------Exports-----------
-module.exports = function SendMSGwithPicture(ctx, imgOptions, msgFunction, picURI, picCaption)
+module.exports = function SendMSGwithPicture(ctx, imgOptions, msgFunction, picURI, picCaption, isanim=false)
 {
     var found = imgOptions.find(function(el) {
         return el.chatid === ctx.from.id;
     });    
     if (found)
     {
-        ctx.replyWithPhoto({source: picURI} , {caption: picCaption}).then(() => { return msgFunction(ctx)} );
+        if (isanim)
+            ctx.replyWithAnimation({source: picURI} , {caption: picCaption}).then(() => { return msgFunction(ctx)} );
+        else 
+            ctx.replyWithPhoto({source: picURI} , {caption: picCaption}).then(() => { return msgFunction(ctx)} );
     }
     else
     {
